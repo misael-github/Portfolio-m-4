aca9f7e4f6fd8fc886e5c2154c11bdf1586e1244
Misael <dmisael2000@gmail.com>
2021-08-18 16:25:07 -0300

    arrangement of links footer

--- componentes/footer/footer.js
+++ componentes/footer/footer.js
@@ -3,9 +3,9 @@ function viewFooter(el) {
     footer.innerHTML= ` <footer class="footer">
     <h2 class="footer__title">Misael</h2>
     <div class="container-links">
-    <a href="https://www.instagram.com/misa.mizrahi/ " class="footer__links"> <i class="fab fa-instagram footer__links--icons"></i>Instagram</a>
-    <a href="https://www.linkedin.com/in/misael-mizrahi-5a4b461b8/" class="footer__links"><i class="fab fa-linkedin-in footer__links--icons"></i>Linkedin</a>
-    <a href="https://github.com/misael-github" class="footer__links"> <i class="fab fa-github footer__links--icons"></i>Github</a>
+    <a href="https://www.instagram.com/misa.mizrahi/ " class="footer__links" target="_blank"> <i class="fab fa-instagram footer__links--icons"></i>Instagram</a>
+    <a href="https://www.linkedin.com/in/misael-mizrahi-5a4b461b8/" class="footer__links" target="_blank"><i class="fab fa-linkedin-in footer__links--icons"></i>Linkedin</a>
+    <a href="https://github.com/misael-github" class="footer__links" target="_blank"> <i class="fab fa-github footer__links--icons"></i>Github</a>
     <div>
   </footer>`
   el.appendChild(footer)
